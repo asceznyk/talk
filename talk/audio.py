77f7aacafffae7b690df29c0b6942036a7e16ca5
asceznyk <asceznyk@gmail.com>
2022-12-07 12:36:03 +0530

    added pad_or_trim

--- talk/audio.py
+++ talk/audio.py
@@ -19,7 +19,17 @@ CHUNK_LENGTH = 30
 N_SAMPLES = CHUNK_LENGTH * SAMPLE_RATE  # 480000: number of samples in a chunk
 N_FRAMES = exact_div(N_SAMPLES, HOP_LENGTH)  # 3000: number of frames in a mel spectrogram input
 
-def load_audio(file: str, sr: int = SAMPLE_RATE):
+def pad_or_trim(array:torch.Tensor, length:int = N_SAMPLES, *, axis:int=-1):
+    if array.shape[axis] > length:
+        array = array.index_select(dim=axis, index=torch.arange(length, device=array.device))
+    elif array.shape[axis] < length:
+        pad_widths = [(0,0)] * array.ndim
+        pad_widths[axis] = [(0, length - array.shape[axis])]
+        array = F.pad(array, [pad for sizes in pad_widths[::-1] for pad in sizes])
+
+    return array
+
+def load_audio(file:str, sr:int = SAMPLE_RATE):
     try: 
         out, _ = (
             ffmpeg.input(file, threads=0)
@@ -36,7 +46,7 @@ def mel_filters(device):
     with np.load(os.path.join(os.path.dirname(__file__), "assets", "mel_filters.npz")) as f:
         return torch.from_numpy(f[f"mel_{N_MELS}"]).to(device)
 
-def log_mel_spec(audio: Union[str, torch.Tensor, np.ndarray]):
+def log_mel_spec(audio:Union[str, torch.Tensor, np.ndarray]):
     if not torch.is_tensor(audio):
         if isinstance(audio, str): audio = load_audio(audio)
         audio = torch.from_numpy(audio)
