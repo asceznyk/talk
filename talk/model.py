9e10806c2809dcf2cce0bd28a24ad01479e53d3c
asceznyk <asceznyk@gmail.com>
2022-12-12 21:34:35 +0530

    added AudioEncoder

--- talk/model.py
+++ talk/model.py
@@ -89,6 +89,8 @@ class ResidualAttentionBlock(nn.Module):
 
 class AudioEncoder(nn.Module):
     def __init__(self, n_mels:int, n_ctx:int, n_state:int, n_head:int, n_layer:int):
+        super().__init__()
+
         self.conv1 = Conv1d(n_mels, n_state, kernel_size=3, padding=1)
         self.conv2 = Conv1d(n_state, n_state, kernel_size=3, stride=2, padding=1)
 
