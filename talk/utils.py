561ed8b78b6daee034ea23f308fcb4624d8be0b6
asceznyk <asceznyk@gmail.com>
2022-12-23 12:26:35 +0530

    added language detection!

--- talk/utils.py
+++ talk/utils.py
@@ -5,5 +5,9 @@ def exact_div(a, b):
     assert a % b == 0
     return a // b
 
+def compression_ratio(text) -> float:
+    text_bytes = text.encode("utf-8")
+    return len(text_bytes) / len(zlib.compress(text_bytes))
+
 
 
