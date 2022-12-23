00446d97f111e4be4bcf9e8adf2cc91e3a02c43e
asceznyk <asceznyk@gmail.com>
2022-12-23 12:41:39 +0530

    updated init.py

--- talk/__init__.py
+++ talk/__init__.py
@@ -1,12 +1,13 @@
-import hashlib
 import io
 import os
 import urllib
+import hashlib
 import warnings
+
+from tqdm import tqdm
 from typing import List, Optional, Union
 
 import torch
-from tqdm import tqdm
 
 from .audio import load_audio, log_mel_spec, pad_or_trim
 #from .decoding import DecodingOptions, DecodingResult, decode, detect_language
