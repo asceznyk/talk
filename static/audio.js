e70153fead0120c735b665b1063684e495fd9184
asceznyk <asceznyk@gmail.com>
2022-12-28 22:46:49 +0530

    added input type=file

--- static/audio.js
+++ static/audio.js
@@ -1,13 +1,19 @@
 const uploadBtn = document.getElementById("upload");
 const audioInp = document.getElementById("audio");
+const audioPlayer = document.getElementById("player");
+const transcriptDiv = document.getElementById("transcript");
 
 async function uploadAudio(e) {
-	let formData = new FormData(); 
+	let formData = new FormData();
+	let inpAudio = audioInp.files[0]
+
 	formData.append("reqtype", "upload");
-	formData.append("audio", audioInp.files[0]);	
+	formData.append("audio", inpAudio);
+	audioPlayer.src = URL.createObjectURL(inpAudio);
+
 	let result = await fetch('/', {method:"POST", body:formData});
 	result = await result.json()
-	console.log(result)
+	transcriptDiv.innerHTML = result.text
 }
 
 uploadBtn.addEventListener("click", uploadAudio);
