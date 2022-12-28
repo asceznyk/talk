d8e931c75577001d783b9d54911dbce732904659
asceznyk <asceznyk@gmail.com>
2022-12-28 12:26:08 +0530

    flask app for talk!

--- home.py
+++ home.py
@@ -20,7 +20,7 @@ def main_page():
         return render_template('main.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
 
 
