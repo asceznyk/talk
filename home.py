2352afe368e596c5afedef355f7e9533970e42b9
asceznyk <asceznyk@gmail.com>
2022-12-28 21:39:49 +0530

    added input type=file

--- home.py
+++ home.py
@@ -4,18 +4,30 @@ import wave
 
 import numpy as np
 
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request
 from pydub import AudioSegment
 
 import talk
 
 app = Flask(__name__)
-#app.config["UPLOAD_DIR"] = "uploaded"
+app.config["UPLOAD_DIR"] = "user_data"
+
+ALLOWED_EXTS = {'wav', 'mp3', 'ogg'}
 
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
+    def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTS
+
     if request.method == 'POST':
-        return False ## will put something in here!
+        if request.form.get('reqtype') == 'upload':
+            file = request.files['file']
+            if file.filename == '':
+                flash("No selected file!") 
+            if file and allowed_file(file.filename):
+                file.save(os.path.join(app.config["UPLOAD_DIR"], filename))
+                print("successfully uploaded audio!")
+            return 
     else:
         return render_template('main.html')
 
--- static/audio.js
+++ static/audio.js
@@ -1,4 +1,15 @@
-const upload = document.getElementById("upload")
+const uploadBtn = document.getElementById("upload");
+const audioInp = document.getElementById("audio");
+
+async function uploadAudio(e) {
+	let formData = new FormData(); 
+	formData.append("reqtype", "upload");
+	formData.append("audio", audioInp.files[0]);
+	console.log(formData);
+	await fetch('/', {method:"POST", body:formData});	
+}
+
+uploadBtn.addEventListner("click", uploadAudio);
 
 
 
