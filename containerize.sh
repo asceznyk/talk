723ab4854a12baffd26b09f7a14edac3dd4ca427
asceznyk <asceznyk@gmail.com>
2023-01-02 11:16:19 +0530

    added env/ to dockerfile..

--- containerize.sh
+++ containerize.sh
@@ -5,7 +5,7 @@ docker rm $(docker ps -a -q)
 docker image remove talktome
 
 docker build -t talktome .
-docker run -p 5000:5000 -t talktome --privileged
+docker run --privileged -p 5000:5000 -t talktome 
 
 
 
