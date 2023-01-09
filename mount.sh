6030cfbb32fc501dbc3576bd91c80dc5084781ca
asceznyk <asceznyk@gmail.com>
2023-01-09 21:30:36 +0530

    live ASR!

--- mount.sh
+++ mount.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 gcsfuse --implicit-dirs whisper_weights ./assets/
-gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 home:app
+gunicorn --certfile=adhoc --bind :$PORT --workers 1 --threads 8 --timeout 0 home:app
 
 
 
