f022d098a890382ec5017af7d0affd56f0c315c5
asceznyk <asceznyk@gmail.com>
2023-01-11 18:43:03 +0530

    testing

--- mount.sh
+++ mount.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 gcsfuse --implicit-dirs whisper_weights ./assets/
-gunicorn --certfile cert.pem --keyfile key.pem --bind :$PORT --workers 1 --threads 8 --timeout 0 home:app
+gunicorn --certfile cert.pem --keyfile key.pem --bind :$PORT --workers 5 --threads 2 --timeout 0 home:app
 
 
 
